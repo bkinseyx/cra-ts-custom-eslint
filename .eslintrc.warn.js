aac2bc5bbf80c8607cd1c18921d99286346caacb
Ben Kinsey <bkinsey@xtivia.com>
2020-03-04 22:44:04 -0600

    fix warn

--- .eslintrc.warn.js
+++ .eslintrc.warn.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['.eslintrc'],
   rules: {
-    'sonarjs/cognitive-complexity': ['error', 2],
+    'sonarjs/cognitive-complexity': ['warn', 2],
     'max-lines-per-function': ['warn', 20],
   },
 };
